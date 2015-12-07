d5116048acf08498c6f60025f859b9471cc8fae1
robin <robinson3020@gmail.com>
2015-12-07 14:26:03 +0800

    001

---
+++ test/src/com/xinyang/constant/Constant.java
@@ -0,0 +1,127 @@
+package com.xinyang.constant;
+
+
+public class Constant {
+	 public static final String salt ="xinyang";
+	 
+	 public static final String NO_DATA_DESC = "no data";
+	 public static final String NO_DATA_CODE = "404";
+	 public static final String SUCCESS_DESC = "ok";
+	 public static final String SUCCESS_CODE = "200";
+	 public static final String FAIL_DESC = "failed";
+	 public static final String FAIL_CODE = "failed"; 
+		
+		
+	    public static final String CODE_SUCCESS ="000";
+	    //璇锋眰澶勭悊澶辫触
+	    public static final String CODE_001 ="001";
+	    //鍙傛暟涓嶅畬鏁�
+	    public static final String CODE_003 ="003";
+	    //鏈熸暟涓嶅瓨鍦�(鎴栨湁璇�)
+	    public static final String CODE_006 ="006";
+	    // 璇ユ湡宸插仠姝㈤攢鍞�
+	    public static final String CODE_007 ="007";
+	    // 璇ユ湡鏈紑濮嬮攢鍞�
+	    public static final String CODE_008 ="008";
+	    //褰╃涓嶈兘涓虹┖
+	    public static final String CODE_009 ="009";
+	    //鍊嶆暟瓒呰繃涓婇檺(鎴栨湁璇�)
+	    public static final String CODE_010 ="010";
+	    //娉ㄦ暟瓒呰繃涓婇檺(鎴栨湁璇�)
+	    public static final String CODE_011 ="011";
+	    //鎶曟敞閲戦瓒呰繃涓婇檺(鎴栨湁璇�)
+	    public static final String CODE_012 ="012";
+	    //鎶曟敞鐨勫唴瀹规牸寮忛敊璇�
+	    public static final String CODE_014 ="014";
+	    //璇ュ僵绉嶇帺娉曚笉瀛樺湪(鎴栨湁璇�)
+	    public static final String CODE_015 ="015";
+	    //瀹㈡埛绔祦姘村彿閲嶅
+	    public static final String CODE_016 ="016";
+	    //	瀹㈡埛绔祦姘村彿涓嶅瓨鍦�(鎴栨湁璇�)
+	    public static final String CODE_017 ="017";
+	    //	璇ユ湡鏆傚仠璐拱
+	    public static final String CODE_018 ="018";
+	    //	娑堟伅娴佹按鍙锋湁璇�
+	    public static final String CODE_019 ="019";
+	    //	璐︽埛浣欓涓嶈冻
+	    public static final String CODE_020 ="020";
+	    //杩藉姞鍙傛暟鏈夎
+	    public static final String CODE_023 ="023";
+	    //璇ョ敤鍗″彿宸插瓨鍦�
+	    public static final String CODE_030 ="030";
+	    //鍗″彿涓嶅厑璁镐负绌�
+	    public static final String CODE_031 ="031";
+	    //瀵嗙爜涓嶅厑璁镐负绌�
+	    public static final String CODE_033 ="033";
+	    //璇ョ敤鍙蜂俊鎭笉瀛樺湪
+	    public static final String CODE_035 ="035";
+	    //瀵嗙爜淇敼澶辫触锛屾棫瀵嗙爜涓嶆纭�
+	    public static final String CODE_040 ="040";
+	    //鐧诲綍澶辫触锛岀敤鎴峰悕鎴栧瘑鐮佹湁璇�
+	    public static final String CODE_041 ="041";
+	    //璇ョ敤鎴峰凡鐧诲綍
+	    public static final String CODE_042 ="042";
+	    //璇ョ敤鎴锋湭鐧诲綍
+	    public static final String CODE_043 ="043";
+	    //鍔犲瘑瀛楃涓查獙璇佹湭閫氳繃
+	    public static final String CODE_076 ="076";
+	    //瀹㈡埛绔紪鍙蜂笉鑳戒负绌�
+	    public static final String CODE_102 ="102";
+	    //瀹㈡埛绔俊鎭笉瀛樺湪
+	    public static final String CODE_103 ="103";
+	    //瀹㈡埛绔痬ac鍦板潃涓嶅尮閰�
+	    public static final String CODE_104 ="104";
+	    //瀹㈡埛绔姸鎬佸凡鍋滅敤
+	    public static final String CODE_105 ="105";
+	    //瀹㈡埛绔姸鎬佸凡閫�鏈�
+	    public static final String CODE_106 ="106";
+	    //璇锋眰鏃堕棿鎴冲け鏁�
+	    public static final String CODE_114 ="114";
+
+	    public static final String PARAM_HEAD_ID = "id";
+	    public static final String PARAM_HEAD_REQTIMESTAMP = "reqtimestamp";
+	    public static final String PARAM_HEAD_TERMINAL_NO = "terminal_no";
+	    public static final String PARAM_HEAD_SIGN = "sign";
+
+	    public static final String PARAM_NOTE = "note";
+	    public static final String PARAM_CODE = "code";
+	    public static final String PARAM_JSESSIONID = "jsessionid";
+	    public static final String PARAM_SYSTEM_TIME = "system_time";
+
+	    public static final String PARAM_SYSTEM = "system";
+	    public static final String PARAM_TERMINAL_NO = "terminal_no";
+	    public static final String PARAM_MAC = "mac";
+	    public static final String PARAM_URL = "url";
+	    public static final String PARAM_VERSION = "ver";
+	    public static final String PARAM_FORCE_UPDATE = "force_update";
+
+	    public static final String PARAM_PAGE_NUM = "page_num";
+	    public static final String PARAM_PAGE_SIZE = "page_size";
+	    public static final String PARAM_TOTAL_PAGE = "total_page";
+	    public static final String PARAM_TOTAL_NUM = "total_num";
+	    public static final String PARAM_ITEM = "item";
+
+	    public static final String PARAM_PRIZE_STATUS = "prize_status";
+	    public static final String PARAM_TYPE = "type";
+	    public static final String PARAM_VIEW_DATE = "view_date";
+	    //
+	    public static final String PARAM_CARD_NUM = "card_num";
+	    public static final String PARAM_CARD_PWD = "card_pwd";
+	    public static final String PARAM_CARD_PWD_NEW = "card_pwd_new";
+	    public static final String PARAM_LOTTERY_PK = "lottery_pk";
+	    public static final String PARAM_LOTTERY_PLAY = "lottery_play";
+	    public static final String PARAM_LOTTERY_BET = "lottery_bet";
+	    public static final String PARAM_PERIOD = "period";
+	    public static final String PARAM_AMOUNT = "amount";
+	    public static final String PARAM_MULTIPLE = "multiple";
+	    public static final String PARAM_NUMBER = "number";
+	    public static final String PARAM_SUP = "sup";
+	    public static final String PARAM_MONEY = "money";
+
+	    public static final String PARAM_BALANCE = "balance";
+	    public static final String PARAM_NEWS_TYPE = "news_type";
+	    public static final String PARAM_STATUS = "status";
+	    public static final String PARAM_CARD_VERIFY_PWD = "card_verify_pwd";
+
+
+}
